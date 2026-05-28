3e80dd745c0e4848867e59ce06341be60e4beec4
sanwrgodara-beep <sanwrgodara@gmail.com>
2026-05-28 19:53:37 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Kisan Mitra Website</h1>
+      <p>Website successfully deployed 🚀</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
